3411b1934316b51983c09d313779d1c52334c527
zzeng31 <zinuozeng@gmail.com>
2023-07-29 12:46:45 -0700

    first commit

---
+++ server/schema/schema.js
@@ -0,0 +1,97 @@
+// const { projects, clients } = require("../sampleData.js");
+
+// Mongoose models
+const Project = require("../models/Project.js");
+const Client = require("../models/Client.js");
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
+
+// Client Type
+
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    email: {
+      type: GraphQLString,
+    },
+    phone: {
+      type: GraphQLString,
+    },
+  }),
+});
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+
+    clientId: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    status: {
+      type: GraphQLString,
+    },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        // return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return Project.find();
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return Client.find();
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Client.findById(args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
